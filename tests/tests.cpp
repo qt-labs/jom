6615563df653e3284b8694df8cd15721c0687dc9
Ryan Pavlik <rpavlik@iastate.edu>
2011-10-31 11:13:22 +0100

    Do a better job of finding jom in the tests
    Merge-request: 8
    Reviewed-by: Joerg Bornemann <joerg.bornemann@nokia.com>

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -838,7 +838,13 @@ bool ParserTest::runJom(const QStringList &args, const QString &workingDirectory
 #else
     const QLatin1String jomBinaryName("jom.exe");
 #endif
-    QString jomBinary = QFileInfo("../../bin/" + jomBinaryName).absoluteFilePath();
+    QString jomBinary = QFileInfo(QCoreApplication::applicationDirPath() + "/" + jomBinaryName).absoluteFilePath();
+	if (!QFile::exists(jomBinary)) {
+		jomBinary = QFileInfo("../../bin/" + jomBinaryName).absoluteFilePath();
+		if (!QFile::exists(jomBinary)) {
+			qDebug("could not find jom");
+		}
+	}
     QString oldWorkingDirectory;
     if (!workingDirectory.isNull()) {
         oldWorkingDirectory = QDir::currentPath();
