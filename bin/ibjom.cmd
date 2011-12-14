035d5fd1021fa9b4739a97445e8a648d4c788ff2
Joerg Bornemann <joerg.bornemann@nokia.com>
2011-12-14 10:57:56 +0100

    fix ibjom.cmd
    Now you can have ibjom / jom in a path with spaces.
    Also, you can set the number of jobs in the environment variable
    IBJOM_NUMBEROFJOBS.

--- bin/ibjom.cmd
+++ bin/ibjom.cmd
@@ -1,1 +1,3 @@
-@xgConsole /profile=%~dp0\profile.xml /command="jom -j 10 %*"
+@echo off
+if "%IBJOM_NUMBEROFJOBS%" == "" set IBJOM_NUMBEROFJOBS=20
+xgConsole /profile="%~dp0\profile.xml" /command="jom -j%IBJOM_NUMBEROFJOBS% %*"
