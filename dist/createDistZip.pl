c91a204b05f97eef3c73aaaba3036e20f79fd487
Joerg Bornemann <joerg.bornemann@nokia.com>
2012-03-12 18:20:17 +0100

    separate version number parts in distribution script
    Change-Id: I1e9b8492437701086f61b0e06984aa6464464ca3
    Reviewed-by: Joerg Bornemann <joerg.bornemann@nokia.com>

--- dist/createDistZip.pl
+++ dist/createDistZip.pl
@@ -2,5 +2,10 @@
 $jomVersion = `../bin/jom.exe /VERSION`;
 $jomVersion = substr($jomVersion, 12);
 $jomVersion = substr($jomVersion, 0, -1);
-$jomVersion =~ s/\.//g;
-system("copy /Y jom.zip jom" . $jomVersion . ".zip");
+$jomVersion =~ /(\d+)\.(\d+)\.(\d+)/;
+$jomVersion = ${1} . "_" . ${2} . "_";
+if (length(${3}) == 1) {
+    $jomVersion = $jomVersion . "0";
+}
+$jomVersion = $jomVersion . ${3};
+system("copy /Y jom.zip jom_" . $jomVersion . ".zip");
