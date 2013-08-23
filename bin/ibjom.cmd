19523cad6cc60f3b4012668e7727ff17c3ebf789
Joerg Bornemann <joerg.bornemann@digia.com>
2013-08-23 15:11:30 +0200

    ibjom: use BuildConsole instead of xgConsole
    IncrediBuild directly supports jom these days.
    The XGE package is not necessary anymore.
    
    Change-Id: I5cc71e972ddda9c23d98d1fbb57cfbd4769f23ab
    Reviewed-by: Joerg Bornemann <joerg.bornemann@digia.com>

--- bin/ibjom.cmd
+++ bin/ibjom.cmd
@@ -1,3 +1,3 @@
 @echo off
 if "%IBJOM_NUMBEROFJOBS%" == "" set IBJOM_NUMBEROFJOBS=20
-xgConsole /profile="%~dp0\profile.xml" /command="jom -j%IBJOM_NUMBEROFJOBS% %*"
+BuildConsole /command="jom -j%IBJOM_NUMBEROFJOBS% %*"
