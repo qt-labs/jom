73f64650288a8e1c6e8bcfc08bbc692da976053a
Joerg Bornemann <joerg.bornemann@theqtcompany.com>
2015-03-30 15:43:48 +0200

    add missing newline for error messages
    Change-Id: I5c9caed3fd7fa82351c3da4121109b7d6e2caf92
    Reviewed-by: Christian Stenger <christian.stenger@theqtcompany.com>

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -182,7 +182,7 @@ int main(int argc, char* argv[])
             fflush(stdout);
         }
     } catch (const Exception &e) {
-        fprintf(stderr, "jom: %s", qPrintable(e.message()));
+        fprintf(stderr, "jom: %s\n", qPrintable(e.message()));
         result = 2;
     }
     return result;
