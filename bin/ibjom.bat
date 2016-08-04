e93bb481016d910201a215ba712278d9cd993565
Joerg Bornemann <joerg.bornemann@qt.io>
2016-08-04 19:41:20 +0200

    Fix ERRORLEVEL propagation in helper batch files
    In ibjom.bat and xgejom.bat ERRORLEVEL wasn't properly propagated to the
    calling script because of the setlocal/endlocal pair that's used within.
    Use the absurd enabledelayedexpansion trick that seems to be the canonical fix.
    
    Task-number: QTCREATORBUG-16619
    Change-Id: Icae4b977309e8a55806fbd3511661366fcefe432
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>

--- bin/ibjom.bat
+++ bin/ibjom.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 if "%IBJOM_NUMBEROFJOBS%" == "" set IBJOM_NUMBEROFJOBS=20
-BuildConsole /command="jom -j%IBJOM_NUMBEROFJOBS% %*"
+BuildConsole /command="jom -j%IBJOM_NUMBEROFJOBS% %*" || exit /b !ERRORLEVEL!
 endlocal
--- bin/xgejom.bat
+++ bin/xgejom.bat
@@ -1,6 +1,6 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 if "%IBJOM_NUMBEROFJOBS%" == "" set IBJOM_NUMBEROFJOBS=60
 set NINJAFLAGS=-j60
-xgConsole /profile="%~dp0\xgejom.xml" /command="jom -j%IBJOM_NUMBEROFJOBS% %*"
+xgConsole /profile="%~dp0\xgejom.xml" /command="jom -j%IBJOM_NUMBEROFJOBS% %*" || exit /b !ERRORLEVEL!
 endlocal
