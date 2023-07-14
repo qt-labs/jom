730224581dd3cd8762b1f41a2d4e05d9ec99f472
Robin Watts <Robin.Watts@artifex.com>
2023-07-14 20:01:05 +0100

    Speed up preselectInferenceRules
    There is no point in repeatedly traversing downwards from a
    node once we've constructed the rules for it.
    
    If a node has rules, just exit.
    
    This vastly reduces the time spent on complicated Makefiles like
    Ghostscript.
    
    Task-number: QTCREATORBUG-29412
    Change-Id: I1607745e46609800fc8ffff1ccafce1979af42eb
    Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- src/jomlib/parser.cpp
+++ src/jomlib/parser.cpp
@@ -684,11 +684,17 @@ QVector<InferenceRule*> Parser::findRulesByTargetName(const QString& targetFileP
 
 void Parser::preselectInferenceRules(DescriptionBlock *target)
 {
-    if (target->m_commands.isEmpty()) {
-        QVector<InferenceRule *> rules = findRulesByTargetName(target->targetName());
-        if (!rules.isEmpty())
-            target->m_inferenceRules = rules;
+    if (!target->m_commands.isEmpty()) {
+        /* If we already have commands for this target, then we've already
+         * generated all the commands for the dependents already. Nothing
+         * more to do. */
+        return;
     }
+
+    QVector<InferenceRule *> rules = findRulesByTargetName(target->targetName());
+    if (!rules.isEmpty())
+        target->m_inferenceRules = rules;
+
     foreach (const QString &dependentName, target->m_dependents) {
         DescriptionBlock *dependent = m_makefile->target(dependentName);
         if (dependent) {
