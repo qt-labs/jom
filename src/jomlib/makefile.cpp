qt-labs/jom

commit 730224581dd3cd8762b1f41a2d4e05d9ec99f472

Speed up preselectInferenceRules
There is no point in repeatedly traversing downwards from a
node once we've constructed the rules for it.

If a node has rules, just exit.

This vastly reduces the time spent on complicated Makefiles like
Ghostscript.

Task-number: QTCREATORBUG-29412
Change-Id: I1607745e46609800fc8ffff1ccafce1979af42eb
Reviewed-by: Oliver Wolff <oliver.wolff@qt.io>
Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

src/jomlib/parser.cpp
@@ -684,11 +684,17 @@ QVector<InferenceRule*> Parser::findRulesByTargetName(const QString& targetFileP
 
 void Parser::preselectInferenceRules(DescriptionBlock *target)
 {
-    if (target->m_commands.isEmpty()) {
-        QVector<InferenceRule *> rules = findRulesByTargetName(target->targetName());
-        if (!rules.isEmpty())
-            target->m_inferenceRules = rules;
+    if (!target->m_commands.isEmpty()) {
+        /* If we already have commands for this target, then we've already
+         * generated all the commands for the dependents already. Nothing
+         * more to do. */
+        return;
     }
+
+    QVector<InferenceRule *> rules = findRulesByTargetName(target->targetName());
+    if (!rules.isEmpty())
+        target->m_inferenceRules = rules;
+
     foreach (const QString &dependentName, target->m_dependents) {
         DescriptionBlock *dependent = m_makefile->target(dependentName);
         if (dependent) {

commit 35261805d99faaad6401cab9c36254fbc135ca1b

Fixes bug introduced in 730224581dd3cd8762b1f41a2d4e05d9ec99f472, when entire subtrees of dependencies were skipped in preselectInferenceRules leading to incorrect build order of dependencies.
Fixed the original performance problem with an extra flag that marks whether a target (and its dependency subtree) has already been visited.

src/jomlib/makefile.cpp
@@ -114,6 +114,7 @@ void Command::evaluateModifiers()
 DescriptionBlock::DescriptionBlock(Makefile* mkfile)
 :   m_bFileExists(false),
     m_bVisitedByCycleCheck(false),
+    m_bVisitedByPreselectInferenceRules(false),
     m_bNoCyclesRootedHere(false),
     m_canAddCommands(ACSUnknown),
     m_pMakefile(mkfile)

src/jomlib/makefile.h
@@ -104,6 +104,7 @@ public:
     bool m_bFileExists;
     bool m_bVisitedByCycleCheck;
     bool m_bNoCyclesRootedHere;
+    bool m_bVisitedByPreselectInferenceRules;
     QVector<InferenceRule*> m_inferenceRules;
 
     enum AddCommandsState { ACSUnknown, ACSEnabled, ACSDisabled };

src/jomlib/parser.cpp
@@ -726,16 +726,17 @@ QVector<InferenceRule*> Parser::findRulesByTargetName(const QString& targetFileP
 
 void Parser::preselectInferenceRules(DescriptionBlock *target)
 {
-    if (!target->m_commands.isEmpty()) {
-        /* If we already have commands for this target, then we've already
-         * generated all the commands for the dependents already. Nothing
-         * more to do. */
+    if (target->m_bVisitedByPreselectInferenceRules)
+        // We already processed this target
         return;
-    }
 
-    QVector<InferenceRule *> rules = findRulesByTargetName(target->targetName());
-    if (!rules.isEmpty())
-        target->m_inferenceRules = rules;
+    target->m_bVisitedByPreselectInferenceRules = true;
+
+    if (target->m_commands.isEmpty()) {
+        QVector<InferenceRule*> rules = findRulesByTargetName(target->targetName());
+        if (!rules.isEmpty())
+            target->m_inferenceRules = rules;
+    }
 
     foreach (const QString &dependentName, target->m_dependents) {
         DescriptionBlock *dependent = m_makefile->target(dependentName);